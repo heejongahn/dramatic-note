69855da161699c078ad4457410ddc1c628aa8d3b
Heejong Ahn <heejongahn@gmail.com>
2016-08-19 22:47:36 +0900

    Touch various dummy files for containers

---
+++ app/assets/js/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const App = ({ children }) => {
+  return (
+    <section>
+      <div id="label-list">
+        label-list
+      </div>
+      <div id="memo-list">
+        memo-list
+      </div>
+      <div id="memo">
+        memo
+      </div>
+    </section>
+  )
+}
+
+export default App;
