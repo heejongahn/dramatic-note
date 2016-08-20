dd4f9baf6a6df4d12c37078b36613130093a6a4a
Heejong Ahn <heejongahn@gmail.com>
2016-08-21 01:21:32 +0900

    Add panel layout for Memo presentational container

--- app/assets/js/components/Memo.js
+++ app/assets/js/components/Memo.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
+const dummyMemos = {
+  "1": {title: "메모1", body: "바디바디당근", checked: false, modifiedAt: Date.now() },
+  "2": {title: "메모2", body: "알레스 클라?", checked: false, modifiedAt: Date.now() },
+  "3": {title: "메모3", body: "드라마앤컴퍼니", checked: false, modifiedAt: Date.now() }
+}
+
 const Memo = ({ params } ) => {
   if (params.memoId) {
-    return <h1>{"Memo id" + params.memoId}</h1>
+    let memo = dummyMemos[params.memoId];
+    return (
+      <div className="panel panel-default memo">
+        <div className="panel-heading">
+          <h3 className="panel-title">{memo.title}</h3>
+        </div>
+        <div className="panel-body">
+          {memo.body}
+          <p>{new Date(memo.modifiedAt).toISOString().slice(0, 10)}</p>
+        </div>
+      </div>
+    )
   } else {
     return null
   }
