84c53d0950aab2e9dda6f88653c2fc211f065e3f
Heejong Ahn <heejongahn@gmail.com>
2016-08-21 01:21:58 +0900

    Redefine redux store spec

--- storeSpec.js
+++ storeSpec.js
@@ -1,9 +1,38 @@
-/* redux store spec for the app */
+/* Redux store spec for the app */
 {
-  labels: {},
-  selectedLabel: null,
+  /*
+   * States below are stored in URL
+   *
+   * labelId: An id of currently selected label
+   * memoId: An id of currently selected memo
+   */
 
-  memos: {},
-  visibleMemoIds: [],
-  checkedMemoIds: []
+  /*
+   * labels: An object storing all labels
+   */
+  labels: {
+    "1": {
+      "name"    : "STRING - Label name",
+      "memoIds" : "[STRING] - A list of memos' Ids related with the label"
+    }
+  }
+
+  /*
+   * memos: An object storing all memos
+   */
+  memos: {
+    "1": {
+      "title"       : "STRING - Memo title",
+      "body"        : "STRING - Memo body",
+      "checked"     : "BOOLEAN - Flag indicating if the memo is checked or not",
+      "modifiedAt"  : "NUMBER - Last modified time in unix time",
+      "labelIds"    : "[STRING] - A list of labels' Ids related with the memo"
+    }
+  }
+
+  /* States below can be calculated using states above
+   *
+   * labelId + memos -> visibleMemos
+   * memos -> selectedMemos
+   */
 }
