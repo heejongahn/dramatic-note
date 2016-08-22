daa6e8ce09876eab844387bbe78cf41691593f79
Heejong Ahn <heejongahn@gmail.com>
2016-08-22 12:39:02 +0900

    Revise reducers.js (debug Object related stuffs)

--- app/assets/js/reducers.js
+++ app/assets/js/reducers.js
@@ -1,62 +1,72 @@
 import { POPULATE, CREATE_LABEL, UPDATE_LABEL, DELETE_LABEL,
-  CREATE_MEMO, UPDATE_MEMO, DELETE_MEMO, TOGGLE_SELECTE_MEMO,
-  ADD_LABELS_TO_MEMOS, REMOVE_LABELS_TO_MEMOS } from './actions'
+  CREATE_MEMO, UPDATE_MEMO, DELETE_MEMO, TOGGLE_SELECT_MEMO,
+  ADD_LABEL_TO_MEMOS, REMOVE_LABEL_FROM_MEMOS } from './actions'
+
+import 'whatwg-fetch'
 
 const initialState = {
-  labels: [],
-  memos: []
+  labels: {},
+  memos: {}
 }
 
 const collectObjects = (objects) => {
   return Object.assign({}, ...objects)
 }
 
-const reducer(state = initialState, action) {
+const reducers = (state = initialState, action) => {
+  let memos, labels, newLabels, newMemos, newMemoIds, memo, label
   switch (action.type) {
     case POPULATE:
-      const memos = action.memos
-      const labels = action.labels
+      memos = action.memos
+      labels = action.labels
 
-      return { memos, labels }
+      return ({ memos, labels })
 
     case CREATE_LABEL:
-      const newLables = collectObjects(state.labels, { action.id: action.label })
-      return collectObjects(state, { labels: newLabels })
+      newLables = collectObjects(
+        state.labels,
+        { [action.id]: action.label }
+      )
+
+      return collectObjects(state, ({ labels: newLabels }))
 
     case UPDATE_LABEL:
-      const newLabels = collecObjects(state.labels, { action.id: actoin.label })
-      return collectObjects(state, { labels: newLabels })
+      newLabels = collectObjects(state.labels,
+        { [action.id] : action.label }
+      )
+
+      return collectObjects(state, ({ labels: newLabels }))
 
     case DELETE_LABEL:
-      const newLabels = Object.keys(state.labels)
+      newLabels = Object.keys(state.labels)
         .filter(id => id != action.id)
         .map(id => state.labels[id])
 
-      const newMemos = Object.keys(state.memos).map(id => {
-        const memo = state.memos[id]
-        const newLabelIds = memo.labelIds.filter(id => id != action.id)
+      newMemos = Object.keys(state.memos).map(id => {
+        memo = state.memos[id]
+        newLabelIds = memo.labelIds.filter(id => id != action.id)
 
-        return collectObjects(memo, { labelIds: newLabelIds })
+        return collectObjects(memo, ({ labelIds: newLabelIds }))
       })
 
-      return collectObjects(state, { labels: newLabels, memos: newMemos })
+      return collectObjects(state, ({ labels: newLabels, memos: newMemos }))
 
     case CREATE_MEMO:
-      const newMemos = collectObject(state.memos, { action.id: action.memo })
+      newMemos = collectObject(state.memos, { [action.id]: action.memo })
       return collectObjects(state, { memos: newMemos })
 
     case UPDATE_MEMO:
-      const newMemos = collectObject(state.memos, { action.id: action.memo })
+      newMemos = collectObject(state.memos, { [action.id]: action.memo })
       return collectObjects(state, { memos: newMemos })
 
     case DELETE_MEMO:
-      const newMemos = Object.keys(state.memos)
+      newMemos = Object.keys(state.memos)
         .filter(id => id != action.id)
         .map(id => state.memos[id])
 
-      const newLabels = Object.keys(state.labels).map(id => {
-        const label = state.labels[id]
-        const newMemoIds = label.memoIds.filter(id => id != action.id)
+      newLabels = Object.keys(state.labels).map(id => {
+        label = state.labels[id]
+        newMemoIds = label.memoIds.filter(id => id != action.id)
 
         return collectObjects(state, { memoIds: newMemoIds })
       })
@@ -64,26 +74,27 @@ const reducer(state = initialState, action) {
       return collectObjects(state, { label: newLabels, memos: newMemos })
 
     case TOGGLE_SELECT_MEMO:
-      const memo = state.memos[action.id]
-      const newMemos = collectObject(state.memos, {
-        action.id: {
+      memo = state.memos[action.id]
+      newMemos = collectObject(state.memos, {
+        [action.id]: {
             title: memo.title,
             body: memo.body,
             selected: !memo.selected,
             modifiedAt: memo.modifiedAt,
             labelIds: memo.labelIds
         }
-      }
+      })
 
       return collectObjects(state, { memos: newMemos })
 
     case ADD_LABEL_TO_MEMOS:
-      const newLabels = collectObject(state.labels, { action.id: action.label })
+      newLabels = collectObject(state.labels,
+        { [action.id]: action.label })
 
-      const newMemos = Object.keys(state.memos).map(id => {
+      newMemos = Object.keys(state.memos).map(id => {
         if (action.label.memoIds.includes(id)) {
-          const memo = state.memos[id]
-          const newLabelIds = memo.labelIds.filter(id => id != action.id)
+          memo = state.memos[id]
+          newLabelIds = memo.labelIds.filter(id => id != action.id)
           newLabelIds.push(action.id)
 
           return collectObjects(memo, { labelIds: newLabelIds })
@@ -95,12 +106,13 @@ const reducer(state = initialState, action) {
       return collectObjects(state, { labels: newLabels, memos: newMemos })
 
     case REMOVE_LABEL_FROM_MEMOS:
-      const newLabels = collectObject(state.labels, { action.id: action.label })
+      newLabels = collectObject(state.labels,
+        { [action.id]: action.label })
 
-      const newMemos = Object.keys(state.memos).map(id => {
+      newMemos = Object.keys(state.memos).map(id => {
         if (action.label.memoIds.includes(id)) {
-          const memo = state.memos[id]
-          const newLabelIds = memo.labelIds.filter(id => id != action.id)
+          memo = state.memos[id]
+          newLabelIds = memo.labelIds.filter(id => id != action.id)
 
           return collectObjects(memo, { labelIds: newLabelIds })
         } else {
@@ -112,3 +124,7 @@ const reducer(state = initialState, action) {
 
     default:
       return state
+  }
+}
+
+export default reducers
