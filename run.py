421e235fa70201d66bedb00466eb6b02afcadbfb
Heejong Ahn <heejongahn@gmail.com>
2016-08-22 16:38:05 +0900

    Add dummy data generating code

--- run.py
+++ run.py
@@ -1,5 +1,32 @@
 from app import app, db
+from app.models import Label, Memo
+
+import random
+import time
 
 if __name__ == "__main__":
+    db.drop_all()
     db.create_all()
+    for i in range(10):
+        label = Label("{}번째 라벨".format(i + 1))
+        db.session.add(label)
+        db.session.commit()
+
+    for i in range(30):
+        memo = Memo("{}번째 메모".format(i + 1),
+                "내용내용내용내용내용내용내용내용내용",
+                int(time.time() * 1000),
+                [])
+        db.session.add(memo)
+        db.session.commit()
+
+    for i in range(100):
+        label = random.choice(Label.query.all())
+        memo = random.choice(Memo.query.all())
+
+        if label not in memo.labels:
+            memo.labels.append(label)
+            db.session.add(memo)
+            db.session.commit()
+
     app.run(debug = True)
