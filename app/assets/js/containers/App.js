46209105463bfbabda7bd3c901ff8bbfc4723aa8
Heejong Ahn <heejongahn@gmail.com>
2016-08-22 16:44:10 +0900

    Replace dummyLabels with acutal label, fetched from server

--- app/assets/js/containers/App.js
+++ app/assets/js/containers/App.js
@@ -7,16 +7,12 @@ import { connect } from 'react-redux'
 
 import LabelContainer from './LabelContainer';
 
-const dummyLabels = {
-  1: { name: "라벨 1", memoIds: [1,2,3,4,5]},
-  2: { name: "라벨 2", memoIds: [2,3,4]},
-  3: { name: "라벨 3", memoIds: [6,4,5,3]}
-}
-
 const App = ({ children, memos, labels }) => {
   return (
     <section className="row">
-      <LabelContainer labels={dummyLabels} />
+      <LabelContainer
+        labels={labels}
+        totalNumMemos={Object.keys(memos).length} />
       { children }
     </section>
   )
