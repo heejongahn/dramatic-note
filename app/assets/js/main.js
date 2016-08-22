d2d1eed3b59c709344666dec4e86695017aea937
Heejong Ahn <heejongahn@gmail.com>
2016-08-22 12:40:54 +0900

    Connect redux, react, redux-thunk

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,25 +1,46 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
+
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
+
 import { App, MemoTab, Memo } from './components';
+import reducers from './reducers'
+import { populate } from './actions'
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 const dummyLabelNames = ["label1", "label2", "label3"]
 const dummyMemoIds = [1, 2, 3]
 
+const store = createStore(
+  reducers,
+  applyMiddleware(thunkMiddleware)
+)
+
+const headers = new Headers({ "Content-type": "application/json" })
+
+fetch('/all_data', { method: 'GET', headers})
+.then(response => response.json())
+.then(result => {
+  return store.dispatch(populate(result['result']))
+})
+
 ReactDom.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={MemoTab} />
-      {dummyLabelNames.map(labelName =>
-        <Route path=":labelName" component={MemoTab}>
-          <IndexRoute component={Memo} />
-          <Route path=":memoId" component={Memo} />
-        </Route>
-      )}
-    </Route>
-  </Router>,
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={MemoTab} />
+        {dummyLabelNames.map(labelName =>
+          <Route path=":labelName" component={MemoTab}>
+            <IndexRoute component={Memo} />
+            <Route path=":memoId" component={Memo} />
+          </Route>
+        )}
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('main')
 );
