710453d7461e3cb00f576869e8e6e203b220ba43
Heejong Ahn <heejongahn@gmail.com>
2016-08-23 01:40:48 +0900

    Add create_label endpoint

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, jsonify, request
 
-from app import app
+from app import app, db
 from app.models import Memo, Label
 
 @app.route('/', defaults={'path': ''})
@@ -8,23 +8,34 @@ from app.models import Memo, Label
 def catch_all(path):
     return render_template('main.html')
 
+def memo_to_json(memo):
+    return { 'title': memo.title, 'body': memo.body,
+            'modifiedAt': memo.modifiedAt,
+            'labelIds': [str(label.id) for label in memo.labels]}
+
+def label_to_json(label):
+    return { 'name': label.name, 'memoIds': [str(memo.id) for memo in label.memos] }
+
 @app.route('/all_data')
 def all_data():
     memo_dict = {}
     for memo in Memo.query.all():
-        memo_dict[memo.id] = {
-                "title": memo.title,
-                "body": memo.body,
-                "modifiedAt": memo.modifiedAt,
-                "labelIds": [str(label.id) for label in memo.labels]
-                }
+        memo_dict[memo.id] = memo_to_json(memo)
 
     label_dict = {}
     for label in Label.query.all():
-        label_dict[label.id] = {
-                "name": label.name,
-                "memoIds": [memo.id for memo in label.memos]
-                }
+        label_dict[label.id] = label_to_json(label)
 
-    result = {"memos": memo_dict, "labels": label_dict}
+    result = {'memos': memo_dict, 'labels': label_dict}
+    return jsonify(result=result)
+
+@app.route('/label', methods=['POST'])
+def create_label():
+    payload = request.get_json()
+    label = Label(payload['name'])
+    db.session.add(label)
+    db.session.commit()
+
+    result = {'id': label.id, 'label': label_to_json(label)}
+
     return jsonify(result=result)
