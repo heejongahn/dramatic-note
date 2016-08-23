c8a6c8bdef689b479183478242f63fc3b1fed8ff
Heejong Ahn <heejongahn@gmail.com>
2016-08-24 02:01:33 +0900

    Add delete memo button to MemoItem

--- app/assets/js/components/MemoItem.js
+++ app/assets/js/components/MemoItem.js
@@ -4,8 +4,7 @@ import React from 'react';
 
 import { Link } from 'react-router';
 import { connect } from 'react-redux'
-
-import { toggleSelectMemo } from '../actions'
+import { deleteMemo, toggleSelectMemo } from '../actions'
 
 const MemoItem = ({ memo, id, labelId, dispatch }) => {
   return (
@@ -18,11 +17,17 @@ const MemoItem = ({ memo, id, labelId, dispatch }) => {
             onChange={()=>dispatch(toggleSelectMemo({ id }))}
           />
         </div>
-        <div className="col-md-10">
+        <div className="col-md-8">
           <Link to={`/${labelId}/${id}`}>
             <b className="memo-item-title">{memo.title}</b>
             <p className="memo-item-preview">{memo.body}</p>
           </Link>
+        </div>
+        <div className="col-md-2">
+          <button className="btn btn-default btn-xs memo-delete"
+            onClick={()=>dispatch(deleteMemo(id))}>
+            삭제
+          </button>
         </div>
       </div>
       <span className="memo-item-date">마지막 수정 날짜: {new Date(memo.modifiedAt).toISOString().slice(0, 10)}</span>
