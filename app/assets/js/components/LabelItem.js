12cd7b821a3b53d0fd3b870a74bee5e06840774f
Heejong Ahn <heejongahn@gmail.com>
2016-08-24 02:46:42 +0900

    Add label update functionality

--- app/assets/js/components/LabelItem.js
+++ app/assets/js/components/LabelItem.js
@@ -4,15 +4,58 @@
 
 // TODO: 이름 변경은 여기 들어가야 할 것 같다
 
-import React from 'react';
-import { Link } from 'react-router';
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router'
 
-const LabelItem = ({ id, label }) => {
+import { updateLabel } from '../actions'
+
+const toggleEditable = (id) => {
+  const labelNameSpan = document.getElementById(`label-name-${id}`)
+  const labelUpdateBtn = document.getElementById(`label-update-${id}`)
+
+  if (labelNameSpan.contentEditable=="true") {
+    labelNameSpan.contentEditable = "false"
+    labelUpdateBtn.style.visibility = "visible"
+  } else {
+    labelNameSpan.contentEditable = "true"
+    labelNameSpan.focus()
+    labelUpdateBtn.style.visibility = "hidden"
+  }
+}
+
+const LabelItem = ({ id, label, dispatch }) => {
+  const onUpdateLabel = () => {
+    const labelNameSpan = document.getElementById(`label-name-${id}`)
+    const newLabelName = labelNameSpan.innerHTML
+
+    dispatch(updateLabel(id, newLabelName))
+    toggleEditable(id)
+  }
+
+  const editLabelButton = (id == 'all')
+    ? null
+    : <button
+        id={`label-update-${id}`}
+        className="btn btn-default btn-xs"
+        onClick={()=>toggleEditable(id)}>
+          수정
+        </button>
+
   return (
     <li className="list-group-item">
-      <Link to={`/${id}`}>{label.name} <span>[{label.numMemos}]</span></Link>
+      <Link to={`/${id}`}>
+        <span
+          id={`label-name-${id}`}
+          className="label-name"
+          onBlur={()=>onUpdateLabel()}>
+          {label.name}
+        </span>
+        <span> [{label.numMemos}]</span>
+      </Link>
+      { editLabelButton }
     </li>
   )
 }
 
-export default LabelItem
+export default connect()(LabelItem)
