d61b1dda52aed31cc60e9a7c1f22a530bc6e075b
Heejong Ahn <heejongahn@gmail.com>
2016-08-23 13:26:56 +0900

    Add all flask endpoints

--- app/views.py
+++ app/views.py
@@ -39,3 +39,81 @@ def create_label():
     result = {'id': label.id, 'label': label_to_json(label)}
 
     return jsonify(result=result)
+
+@app.route('/label/<id>', methods=['PUT', 'DELETE'])
+def edit_or_delete_label(id):
+    label = Label.query.get(id)
+
+    if request.method == 'PUT':
+        payload = request.get_json()
+        label.name = payload['name']
+
+        db.session.add(label)
+        db.session.commit()
+
+        result = {'id': id, 'label': label_to_json(label)}
+
+        return jsonify(result=result)
+
+    else:
+        db.session.delete(label)
+        db.session.commit()
+
+        result = {'id': id}
+
+@app.route('/memo', methods=['POST'])
+def create_memo():
+    payload = request.get_json()
+    memo = Memo(payload['title'], payload['body'], payload['modifiedAt'], [])
+    db.session.add(memo)
+    db.session.commit()
+
+    result = {'id': label.id, 'memo': memo_to_json(memo)}
+
+    return jsonify(result=result)
+
+@app.route('/memo/<id>', methods=['PUT', 'DELETE'])
+def edit_or_delete_memo(id):
+    memo = Memo.query.get(id)
+
+    if request.method == 'PUT':
+        payload = request.get_json()
+        memo.title = payload['title']
+        memo.body = payload['body']
+        memo.modifiedAt = payload['modifiedAt']
+
+        db.session.add(memo)
+        db.session.commit()
+
+        result = {'id': id, 'memo': memo_to_json(memo)}
+
+        return jsonify(result=result)
+
+    else:
+        db.session.delete(memo)
+        db.session.commit()
+
+        result = {'id': id}
+
+@app.route('/label/<labelId>/memos', methods=['POST', 'DELETE'])
+def add_or_remove_label(labelId):
+    payload = request.get_json()
+    label = Label.query.get(labelId)
+    memos = [Memo.query.get(memo_id) for memo_id in payload['memoIds']]
+
+    if request.method == 'POST':
+        for memo in memos:
+            if memo not in label.memos:
+                label.memos.append(memo)
+                db.session.add(memo)
+
+    else:
+        for memo in memos:
+            if memo in label.memos:
+                label.memos.remove(memo)
+                db.session.add(memo)
+
+    db.session.add(label)
+    db.session.commit()
+
+    result = {'id': labelId, 'label': label_to_json(label)}
