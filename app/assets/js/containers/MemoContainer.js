714ab0cb80e7ea402246089591445a0285261537
Heejong Ahn <heejongahn@gmail.com>
2016-08-23 19:38:51 +0900

    Only show memo at MemoPanel if visible

--- app/assets/js/containers/MemoContainer.js
+++ app/assets/js/containers/MemoContainer.js
@@ -4,7 +4,10 @@ import MemoItem from '../components/MemoItem'
 
 const MemoContainer = ({ children, params, memos }) => {
   const labelId = params.labelId
-  const MemoPanel = Object.keys(memos).includes(params.memoId)
+  const visibleMemos = Object.keys(memos)
+    .filter(id => labelId == "all" || memos[id].labelIds.includes(labelId))
+
+  const MemoPanel = Object.keys(visibleMemos).includes(params.memoId)
     ? React.cloneElement(children, { memo: memos[params.memoId] })
     : children
 
@@ -12,9 +15,10 @@ const MemoContainer = ({ children, params, memos }) => {
     <div id="memo-tab" className="col-md-8">
       <div id="memo-list" className="col-md-6">
         <ul className="list-group">
-          {Object.keys(memos)
-            .filter(id => labelId == "all" || memos[id].labelIds.includes(labelId))
-            .map(id => <MemoItem key={id} memo={memos[id]} to={`/${labelId}/${id}`} />) }
+          {visibleMemos
+            .map(id =>
+              <MemoItem key={id} memo={memos[id]} to={`/${labelId}/${id}`} />
+            )}
         </ul>
       </div>
       <div id="memo" className="col-md-6">
