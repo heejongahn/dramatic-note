64c8fd2ac64b50dbaf024aedc232df943d13b856
Heejong Ahn <heejongahn@gmail.com>
2016-08-24 17:10:15 +0900

    Add client-side form validation to AddLabelItem

--- app/src/containers/AddLabelItem.js
+++ app/src/containers/AddLabelItem.js
@@ -4,19 +4,31 @@ import { connect } from 'react-redux'
 
 import { createLabel } from '../actions'
 
-const AddLabelItem = ({ dispatch }) => {
+const AddLabelItem = ({ labelNames, dispatch }) => {
   const onCreateLabel = () => {
     const newLabelForm = document.getElementById('new-label-name')
+    const newLabelFormGroup = document.getElementById('new-label-form-group')
+    const newLabelHelper = document.getElementById('new-label-helper')
+
     const newLabelName = newLabelForm.value
-    newLabelForm.value = ""
-    return dispatch(createLabel(newLabelName))
+
+    if (labelNames.includes(newLabelName)) {
+      newLabelHelper.innerHTML = "이미 존재하는 라벨 이름입니다."
+      newLabelFormGroup.className = "form-group col-md-9 has-error"
+    } else {
+      newLabelForm.value = ""
+      newLabelHelper.innerHTML = ""
+      newLabelFormGroup.className = "form-group col-md-9"
+      return dispatch(createLabel(newLabelName))
+    }
   }
 
   return (
     <li className="list-group-item">
       <form className="form-inline row">
-        <div className="form-group col-md-9">
+        <div id="new-label-form-group" className="form-group col-md-9">
           <input type="text" className="form-control" id="new-label-name" placeholder="새 라벨 이름"/>
+          <span id="new-label-helper" className="help-block"></span>
         </div>
         <button type="submit"
           className="btn btn-default col-md-2"
@@ -31,4 +43,11 @@ const AddLabelItem = ({ dispatch }) => {
   )
 }
 
-export default connect()(AddLabelItem)
+const mapStateToProps = (state, ownProps) => (
+  Object.assign({},
+    { labelNames: Object.keys(state.labels).map(id => state.labels[id].name) },
+    ownProps
+  )
+)
+
+export default connect(mapStateToProps)(AddLabelItem)
