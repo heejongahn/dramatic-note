0b36e60d98f6167152e959ff03b54694b545fa6e
Heejong Ahn <heejongahn@gmail.com>
2016-08-26 02:08:32 +0900

    Seperate run and run_with_dummy

--- run.py
+++ run.py
@@ -5,28 +5,9 @@ import random
 import time
 
 if __name__ == "__main__":
-    db.drop_all()
-    db.create_all()
-    for i in range(10):
-        label = Label("{}번째 라벨".format(i + 1))
-        db.session.add(label)
-        db.session.commit()
-
-    for i in range(30):
-        memo = Memo("{}번째 메모".format(i + 1),
-                "내용내용내용내용내용내용내용내용내용",
-                int(time.time() * 1000 * 0.95),
-                [])
-        db.session.add(memo)
-        db.session.commit()
-
-    for i in range(100):
-        label = random.choice(Label.query.all())
-        memo = random.choice(Memo.query.all())
-
-        if label not in memo.labels:
-            memo.labels.append(label)
-            db.session.add(memo)
-            db.session.commit()
+    try:
+        db.create_all()
+    except:
+        pass
 
     app.run(debug = True)
