0e0be412cfe15704286f5107c621a125bb45b7f0
Heejong Ahn <heejongahn@gmail.com>
2016-08-26 02:40:52 +0900

    Improve EmptyMemoList helper text

--- app/src/components/EmptyMemoList.js
+++ app/src/components/EmptyMemoList.js
@@ -5,7 +5,7 @@ const EmptyMemoList = () => {
     <div id="empty-memo-list" className="panel panel-default memo-panel">
       <div className="jumbotron">
         <h1>메모 없음</h1>
-        <p>이 라벨이 붙어 있는 메모가 없습니다. 지금 바로 라벨을 붙여보세요!</p>
+        <p>이 라벨이 붙어 있는 메모가 없습니다. 지금 바로 새로운 메모를 생성하거나, 라벨을 붙여보세요!</p>
       </div>
     </div>
   )
