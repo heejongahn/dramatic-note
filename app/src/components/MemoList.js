a0fced3415a9a87c80555c4d3ef3b4af53223608
Heejong Ahn <heejongahn@gmail.com>
2016-08-25 16:42:52 +0900

    Show EmptyMemoList when no memo is visible

--- app/src/components/MemoList.js
+++ app/src/components/MemoList.js
@@ -4,11 +4,15 @@ import MemoItem from './MemoItem'
 import EmptyMemoList from './EmptyMemoList'
 
 const MemoList = ({ labelId, memos }) => {
-  return <ul className="list-group">
-    {Object.keys(memos).map(id =>
-      <MemoItem key={id} id={id} memo={memos[id]} labelId={labelId} />)
-    }
-  </ul>
+  if (Object.keys(memos).length == 0) {
+    return <EmptyMemoList />
+  } else {
+    return <ul className="list-group">
+      {Object.keys(memos).map(id =>
+        <MemoItem key={id} id={id} memo={memos[id]} labelId={labelId} />)
+      }
+    </ul>
+  }
 }
 
 export default MemoList
